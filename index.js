a2becf525ac5806b416eb1357e76eea3defd1d0c
futurist <26634873@qq.com>
2016-06-07 17:19:52 +0800

    ADD: combinedStream to combine files

--- index.js
+++ index.js
@@ -1,53 +1,41 @@
-// from http://stackoverflow.com/questions/24976123/streaming-a-video-file-to-an-html5-video-player-with-node-js-so-that-the-video-c
+// Join splitted mp4 files into one stream and feed html5 video
 
-var fs = require('fs'),
-    http = require('http'),
-    url = require('url'),
-    path = require('path')
+var fs = require('fs')
+var http = require('http')
+var path = require('path')
+var CombinedStream = require('combined-stream2')
+var leftPad = require('left-pad')
 
 http.createServer(function (req, res) {
   if (req.url != '/movie.mp4') {
     res.writeHead(200, { 'Content-Type': 'text/html' })
     res.end('<video src="http://localhost:8888/movie.mp4" controls></video>')
   } else {
-    var file = path.resolve(__dirname, 'data/A.001')
-    fs.stat(file, function (err, stats) {
-      if (err) {
-        if (err.code === 'ENOENT') {
-          // 404 Error if file not found
-          return res.sendStatus(404)
-        }
-        res.end(err)
-      }
-      var range = req.headers.range
-      if (!range) {
-        // 416 Wrong range
-        return res.sendStatus(416)
-      }
-      var positions = range.replace(/bytes=/, '').split('-')
-      var start = parseInt(positions[0], 10)
-      var total = stats.size
-      var end = positions[1] ? parseInt(positions[1], 10) : total - 1
-      var chunksize = 1024000 // (end - start) + 1
-      console.log(range, total, positions, start, end, chunksize)
+    var size = 19738853
+    var range = req.headers.range
+    if (!range) {
+      // 416 Wrong range
+      return res.sendStatus(416)
+    }
+    var positions = range.replace(/bytes=/, '').split('-')
+    var start = parseInt(positions[0], 10)
+    var total = size
+    var end = positions[1] ? parseInt(positions[1], 10) : total - 1
+    var chunksize = (end - start) + 1
+    console.log(range, total, positions, start, end, chunksize)
 
-      res.writeHead(206, {
-        'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunksize,
-        'Content-Type': 'video/mp4'
-      })
-
-      var stream = fs.createReadStream(file)
-            .on('open', function () {
-              stream.pipe(res)
-            })
-            .on('error', function (err) {
-              res.end(err)
-            })
-            .on('end', function(err){
-              console.log('end')
-            })
+    res.writeHead(206, {
+      'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4'
     })
+
+    var rs = CombinedStream.create()
+
+    for (var i = 1; i <= 20; i++)
+      rs.append(fs.createReadStream('data/A.' + leftPad(i, 3, 0)))
+
+    rs.pipe(res)
   }
 }).listen(8888)
